9a227374fabd7ba899fc263ce5ce32b07932e3ff
Sergey <goosewww010@gmail.com>
2025-02-27 21:26:05 +0400

    refactor userdto

--- src/main/java/dev/invest/model/user/UserDto.java
+++ src/main/java/dev/invest/model/user/UserDto.java
@@ -2,5 +2,8 @@ package dev.invest.model.user;
 
 import java.util.UUID;
 
-public record UserDto(UUID uuid, String username, String email) {
+public record UserDto(
+        UUID uuid, 
+        String username,
+        String email) {
 }
