580652d0f9fb4ef20bff1aa8c6389925d6a4f069
Sergey <goosewww010@gmail.com>
2025-02-28 10:06:15 +0400

    update forecast dto and requests

--- src/main/java/dev/invest/model/forecast/CreateForecastRequest.java
+++ src/main/java/dev/invest/model/forecast/CreateForecastRequest.java
@@ -1,6 +1,17 @@
 package dev.invest.model.forecast;
 
-public record CreateForecastRequest(
+import jakarta.validation.constraints.NotBlank;
+import java.math.BigDecimal;
 
+public record CreateForecastRequest(
+        @NotBlank String currency,
+        @NotBlank String ticker,
+        Integer recommendation,
+        BigDecimal current_price,
+        BigDecimal consensus,
+        BigDecimal min_target,
+        BigDecimal max_target,
+        BigDecimal price_change,
+        BigDecimal price_change_rel
 ) {
 }
--- src/main/java/dev/invest/model/forecast/ForecastDto.java
+++ src/main/java/dev/invest/model/forecast/ForecastDto.java
@@ -1,5 +1,20 @@
 package dev.invest.model.forecast;
 
-public record ForecastDto() {
+import java.math.BigDecimal;
+import java.util.UUID;
+
+// https://russianinvestments.github.io/investAPI/instruments/#getforecastresponseconsensusitem
+public record ForecastDto(
+        UUID uuid,
+        String currency,
+        String ticker,
+        Integer recommendation,
+        BigDecimal current_price,
+        BigDecimal consensus,
+        BigDecimal min_target,
+        BigDecimal max_target,
+        BigDecimal price_change,
+        BigDecimal price_change_rel
+) {
 
 }
